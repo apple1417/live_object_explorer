c87993b840a86304956bd72f69abb09edc131c4c
apple1417 <git@apple1417.dev>
2025-07-25 21:55:37 +1200

    inital template

---
+++ src/dllmain.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+
+namespace {
+
+HMODULE this_module;
+
+/**
+ * @brief Main startup thread.
+ * @note Instance of `LPTHREAD_START_ROUTINE`.
+ *
+ * @return unused.
+ */
+DWORD WINAPI startup_thread(LPVOID /*unused*/) {
+    return 1;
+}
+
+}  // namespace
+
+/**
+ * @brief Main entry point.
+ *
+ * @param h_module Handle to module for this dll.
+ * @param ul_reason_for_call Reason this is being called.
+ * @return True if loaded successfully, false otherwise.
+ */
+// NOLINTNEXTLINE(misc-use-internal-linkage, readability-identifier-naming)
+BOOL APIENTRY DllMain(HMODULE h_module, DWORD ul_reason_for_call, LPVOID /*unused*/) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+            this_module = h_module;
+            DisableThreadLibraryCalls(h_module);
+            CreateThread(nullptr, 0, &startup_thread, nullptr, 0, nullptr);
+            break;
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
+
